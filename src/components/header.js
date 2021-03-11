d91de62f3669ebbe8a2b8e36faf1836658abfe89
Peter De Clercq <dclercqpeter@gmail.com>
2021-03-11 15:56:23 +0100

    Added Oishii logo
    Added Oishii logo to the Header component

--- src/components/header.js
+++ src/components/header.js
@@ -2,9 +2,23 @@ import React from "react"
 import { Link } from "gatsby"
 import styled from "styled-components"
 
+import { images } from "../constants"
+
 const Header = () => {
+  const HeaderWrapper = styled.div`
+    display: flex;
+    background-color: rgba(0, 0, 0, 0.9);
+  `
+
+  const Header = styled.header`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 1500px;
+    margin: 0 auto;
+  `
+
   const Nav = styled.nav`
-    background: black;
     height: 80px;
     display: flex;
     justify-content: center;
@@ -23,24 +37,38 @@ const Header = () => {
     border-bottom: 2px solid rgba(255, 255, 255, 0);
   `
 
+  const Logo = styled.img`
+    margin-left: 20px;
+  `
+
   return (
-    <Nav>
-      <NavLink id="link-home" to="/">
-        home
-      </NavLink>
-      <NavLink id="link-menu" to="/menu">
-        menu
-      </NavLink>
-      <NavLink id="link-recensies" to="/recensies">
-        recensies
-      </NavLink>
-      <NavLink id="link-contact" to="/contact">
-        contact
-      </NavLink>
-      <NavLink id="link-download" to="/downloadapp">
-        download onze app
-      </NavLink>
-    </Nav>
+    <HeaderWrapper>
+      <Header>
+        <Logo
+          src={images.oishii_logo.default}
+          alt="Oishii Logo"
+          srcset=""
+          height="60"
+        />
+        <Nav>
+          <NavLink id="link-home" to="/">
+            home
+          </NavLink>
+          <NavLink id="link-menu" to="/menu">
+            menu
+          </NavLink>
+          <NavLink id="link-recensies" to="/recensies">
+            recensies
+          </NavLink>
+          <NavLink id="link-contact" to="/contact">
+            contact
+          </NavLink>
+          <NavLink id="link-download" to="/downloadapp">
+            download onze app
+          </NavLink>
+        </Nav>
+      </Header>
+    </HeaderWrapper>
   )
 }
 
