59419514e214fc0269cfaa5bf1cb39e157a31ca8
Peter De Clercq <dclercqpeter@gmail.com>
2021-03-11 15:15:47 +0100

    Update header.js
    Navigation items added:
    - home
    - menu
    - recensies
    - contact
    - download onze app

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,47 @@
-import * as React from "react"
+import React from "react"
 import { Link } from "gatsby"
+import styled from "styled-components"
 
-const Header = () => <div></div>
+const Header = () => {
+  const Nav = styled.nav`
+    background: black;
+    height: 80px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  `
+
+  const NavLink = styled(Link)`
+    color: white;
+    text-decoration: none;
+    font-size: 1.6em;
+    font-family: "Lato";
+    font-weight: bold;
+    text-transform: uppercase;
+    padding: 12px 0;
+    margin: 0 50px;
+    border-bottom: 2px solid rgba(255, 255, 255, 0);
+  `
+
+  return (
+    <Nav>
+      <NavLink id="link-home" to="/">
+        home
+      </NavLink>
+      <NavLink id="link-menu" to="/">
+        menu
+      </NavLink>
+      <NavLink id="link-recensies" to="/">
+        recensies
+      </NavLink>
+      <NavLink id="link-contact" to="/">
+        contact
+      </NavLink>
+      <NavLink id="link-download" to="/">
+        download onze app
+      </NavLink>
+    </Nav>
+  )
+}
 
 export default Header
