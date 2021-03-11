63e68e5729c3a6c2e94200a3f35b0dfbdf3a9628
Peter De Clercq <dclercqpeter@gmail.com>
2021-03-11 14:03:20 +0100

    Constants folder + index/images file added

---
+++ src/constants/images.js
@@ -0,0 +1,7 @@
+const oishii_logo = require("./../assets/images/oishii_logo.png")
+const oishii_logo_x2 = require("./../assets/images/oishii_logo_x2.png")
+
+export default {
+  oishii_logo,
+  oishii_logo_x2,
+}
