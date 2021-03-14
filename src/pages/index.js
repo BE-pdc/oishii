b4b9cda79fbf33c57ff76ac74110e04cf49db49c
Peter De Clercq <dclercqpeter@gmail.com>
2021-03-14 20:32:17 +0100

    files names + imports updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import Layout from "../components/layout"
+import Layout from "../components/Layout"
 import Hero from "./../components/Hero"
 import SEO from "../components/seo"
 
